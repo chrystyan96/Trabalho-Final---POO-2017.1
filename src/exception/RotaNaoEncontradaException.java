bf7530ab20b544b3a747ab37c1ba53a60c505041
Chrystyan Byrnes Santos de Olinda <chrystyan96@hotmail.com>
2017-10-26 22:04:56 -0300

    insersão automatica no banco

--- src/exception/RotaNaoEncontradaException.java
+++
@@ -1,5 +0,0 @@
-package exception;
-
-public class RotaNaoEncontradaException extends Exception{
-
-}
--- src/interfaceUI/IRotas.java
+++
@@ -1,9 +0,0 @@
-package interfaceUI;
-
-import dominio.Localidade;
-
-public interface IRotas {
-
-	public abstract double calcularRotas(Localidade origem, Localidade destino, double peso);
-	
-}
